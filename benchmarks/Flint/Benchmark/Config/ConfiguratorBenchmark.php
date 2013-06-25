d25065707314e7b2ee149e4c4c31f900d77a62b6
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-06-25 16:15:24 +0200

    add benchmark

---
+++ benchmarks/Flint/Benchmark/Config/ConfiguratorBenchmark.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Flint\Benchmark\Config;
+
+use Flint\Config\Configurator;
+use Flint\Config\Normalizer\ChainNormalizer;
+use Flint\Config\Loader\JsonFileLoader;
+use Silex\Application;
+use Symfony\Component\Config\FileLocator;
+
+class ConfiguratorBenchmark extends \Athletic\AthleticEvent
+{
+    public function setUp()
+    {
+        $locator = new FileLocator(array(
+            __DIR__ . '/../../../../tests/Flint/Tests/Fixtures',
+        ));
+
+        $this->app = new Application(array(
+            'debug'            => false,
+            'config.cache_dir' => sys_get_temp_dir(),
+        ));
+
+        // Create configurator and warmup cache
+        $this->configurator = new Configurator(new JsonFileLoader($locator, new ChainNormalizer()));
+        $this->configurator->load($this->app, 'config.json');
+    }
+
+    /**
+     * @iterations 1000
+     */
+    public function loadConfigFile()
+    {
+        $this->configurator->load($this->app, 'config.json');
+    }
+}
