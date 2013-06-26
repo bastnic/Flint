eabb446ed277c2fde97f182ee2f3250c7f775f3c
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-06-26 14:07:08 +0200

    Add more benchmarks

--- benchmarks/Flint/Benchmark/Config/ConfiguratorBenchmark.php
+++ benchmarks/Flint/Benchmark/Config/ConfiguratorBenchmark.php
@@ -5,32 +5,61 @@ namespace Flint\Benchmark\Config;
 use Flint\Config\Configurator;
 use Flint\Config\Loader\JsonFileLoader;
 use Flint\Config\Normalizer\ChainNormalizer;
+use Flint\Config\Normalizer\PimpleAwareNormalizer;
+use Flint\Config\Normalizer\EnvironmentNormalizer;
 use Pimple;
 use Symfony\Component\Config\FileLocator;
 
 class ConfiguratorBenchmark extends \Athletic\AthleticEvent
 {
-    public function setUp()
+    public function classSetUp()
     {
-        $locator = new FileLocator(array(
-            __DIR__ . '/../../../../tests/Flint/Tests/Fixtures',
+        $this->pimple = new Pimple(array(
+            'root_dir' => __DIR__,
+            'debug' => true,
         ));
 
-        $this->pimple = new Pimple;
+        $normalizers = new ChainNormalizer(array(
+            new PimpleAwareNormalizer($this->pimple),
+            new EnvironmentNormalizer(),
+        ));
 
-        // make sure a previous run isnt interfering
-        @unlink(sys_get_temp_dir() . '/' . crc32('config.json') . '.php');
+        $loader = new JsonFileLoader($normalizers, new FileLocator(__DIR__ . '/../Fixtures'));
 
         // Create configurator and warmup cache
-        $this->configurator = new Configurator(new JsonFileLoader(new ChainNormalizer, $locator), sys_get_temp_dir());
-        $this->configurator->configure($this->pimple, 'config.json');
+        $this->cached = new Configurator($loader, sys_get_temp_dir());
+        $this->cached->configure($this->pimple, 'inherit.json');
+
+        $this->nonCached = new Configurator($loader, null, false);
+    }
+
+    public function classTearDown()
+    {
+        // make sure a previous run isnt interfering
+        @unlink(sys_get_temp_dir() . '/' . crc32('inherit.json') . '.php');
     }
 
     /**
      * @iterations 10000
      */
-    public function loadConfigFile()
+    public function loadCachedConfigFile()
     {
-        $this->configurator->configure($this->pimple, 'config.json');
+        $this->cached->configure($this->pimple, 'inherit.json');
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function loadNonCachedConfigFile()
+    {
+        $this->nonCached->configure($this->pimple, 'inherit.json');
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function loadSimpleNonCachedConfigFile()
+    {
+        $this->nonCached->configure($this->pimple, 'simple.json');
     }
 }
